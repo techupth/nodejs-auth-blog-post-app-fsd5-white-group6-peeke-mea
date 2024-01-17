ad8193d95d2cffdd9b87ecd2765fa5cf56231a90
Gravizz <peeke0rr@gmail.com>
2024-01-17 14:03:30 +0700

    feat:add register api

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import postRouter from "./apps/posts.js";
 import { client } from "./utils/db.js";
+import authRouter from "./apps/auth.js";
 
 async function init() {
   const app = express();
--- server/apps/auth.js
+++ server/apps/auth.js
@@ -4,7 +4,21 @@ const authRouter = Router();
 
 // 🐨 Todo: Exercise #1
 // ให้สร้าง API เพื่อเอาไว้ Register ตัว User แล้วเก็บข้อมูลไว้ใน Database ตามตารางที่ออกแบบไว้
+authRouter.post("/register", async (req, res) => {
+  try {
+    const user = {
+      username: req.body.username,
+      password: req.body.password,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+    };
 
+    const collection = db.collection("user");
+    await collection.insertOne(user);
+  } catch (error) {
+    return res.json({ message: `${error}` });
+  }
+});
 // 🐨 Todo: Exercise #3
 // ให้สร้าง API เพื่อเอาไว้ Login ตัว User ตามตารางที่ออกแบบไว้
 
