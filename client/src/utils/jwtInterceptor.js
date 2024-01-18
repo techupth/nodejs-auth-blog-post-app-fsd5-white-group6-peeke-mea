da511b22bf09163794cfb8e210c90c2da2bc2dbe
Gravizz <peeke0rr@gmail.com>
2024-01-18 17:03:33 +0700

    feat:add logout function

--- client/src/utils/jwtInterceptor.js
+++ client/src/utils/jwtInterceptor.js
@@ -6,6 +6,17 @@ function jwtInterceptor() {
     //  ให้เขียน Logic ในการแนบ Token เข้าไปใน Header ของ Request
     // เมื่อมีการส่ง Request จาก Client ไปหา Server
     // ภายใน Callback Function axios.interceptors.request.use
+    axios.interceptors.request.use((req) => {
+      const hasToken = Boolean(window.localStorage.getItem("token"));
+
+      if (hasToken) {
+        req.headers = {
+          ...req.headers,
+          Authorization: `Bearer ${window.localStorage.getItem("token")}`,
+        };
+      }
+      return req;
+    });
 
     return req;
   });
@@ -19,6 +30,13 @@ function jwtInterceptor() {
       //  ให้เขียน Logic ในการรองรับเมื่อ Server ได้ Response กลับมาเป็น Error
       // โดยการ Redirect ผู้ใช้งานไปที่หน้า Login และลบ Token ออกจาก Local Storage
       // ภายใน Error Callback Function ของ axios.interceptors.response.use
+      if (
+        error.response.status === 401 &&
+        error.response.statusText === "Unauthorized"
+      ) {
+        window.localStorage.removeItem("token");
+        window.location.replace("/");
+      }
 
       return Promise.reject(error);
     }
